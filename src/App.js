e8369a67c8a8041d435d995d826c226843c0160a
AdraProductStudio <adraforyou@gmail.com>
2025-01-31 10:12:22 +0530

    CREATION : Consumewise page project  creation

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
+import { ToastContainer } from "react-toastify";
+import { InitializeProjectSetup } from "Views/Common/Docs/InitializeProjectSetup";
+import Login from "Views/Common/Docs/Login";
+import Error from "Views/Common/Docs/error";
+import InterviewCandidatesRegistration from "Views/InterviewCandidates/Docs/InterviewCandidatesRegistration";
+import InterviewCandidatesHome from "Views/InterviewCandidates/Docs/InterviewCandidatesHome";
+import InterviewCandidatesAuth from "Views/InterviewCandidates/Docs/InterviewCandidatesAuth";
+import CandidateStatus from "CandidateStatus";
+import AdminAuth from "Views/Admin/Docs/AdminAuth";
+import Layout from "Views/Admin/Layout/Layout";
+import Home from "Home";
+import ChatPage from "ChatPage";
+
+
+const App = () => {
+
+  return (
+    <HelmetProvider>
+      <ToastContainer theme='light' />
+      <Routes>
+
+        
+        <Route element={<InitializeProjectSetup />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/chat" element={<ChatPage />} />
+         
+
+          <Route path="*" element={<Error />} />
+        </Route>
+      </Routes>
+    </HelmetProvider >
+  )
+}
+export default App;
